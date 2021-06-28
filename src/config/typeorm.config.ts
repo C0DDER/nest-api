82533ab366115a8bd969e1a14974ba2b22cfafba
codder <1111alex11189@gmail.com>
2021-06-28 13:15:33 +0300

    db fix

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const TypeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'ec2-35-169-188-58.compute-1.amazonaws.com',
-  port: 5432,
-  username: 'rjtmcvcoiizhzv',
-  password: '0697166c2aa5cad32ca85ec1fcafb468e8043529ecd1b80e77c1d0406182bb34',
-  database: 'd317018hoql2rn',
+  url:
+    'postgres://rjtmcvcoiizhzv:0697166c2aa5cad32ca85ec1fcafb468e8043529ecd1b80e77c1d0406182bb34@ec2-35-169-188-58.compute-1.amazonaws.com:5432/d317018hoql2rn',
   autoLoadEntities: true,
   synchronize: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
