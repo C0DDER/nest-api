7a4d04a273adaab66269c364806fe43061298e33
codder <1111alex11189@gmail.com>
2021-06-28 11:33:58 +0300

    db config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const TypeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'ec2-35-169-188-58.compute-1.amazonaws.com',
   port: 5432,
-  username: 'leha',
-  password: 'root',
-  database: 'taskmanagement',
+  username: 'rjtmcvcoiizhzv',
+  password: '0697166c2aa5cad32ca85ec1fcafb468e8043529ecd1b80e77c1d0406182bb34',
+  database: 'd317018hoql2rn',
   autoLoadEntities: true,
   synchronize: true,
 };
